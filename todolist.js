7637dba52543b6feb5f2bcc11a786c5ccf63cc5f
Arpit Jain <arpit2126@gmail.com>
2015-10-12 00:56:10 +0530

    creation of todolist object

--- todolist.js
+++ todolist.js
@@ -1,1 +1,38 @@
+var TodoList = function(){
 
+		 		var tasks = [];
+		 		var counter = 0;
+		 		var Task = function(description){
+				 	this.id = counter+1;
+				 	this.description = description;
+				 	this.completed = false;
+				};
+
+				Task.prototype.complete = function(){
+				  	this.completed = true;
+				};
+
+				var removeTask = function(index){
+				  	tasks.splice(index, 1);
+				};
+
+				var addTask = function(description){
+					var task = new Task(description);
+					tasks.push(task);
+				};
+
+				var completeTask = function(task){
+     				task.complete();
+				};
+
+				var taskList = function(){
+					return tasks;
+				}
+
+				return {
+					addTask : addTask,
+					removeTask:removeTask,
+					taskList:taskList,
+					completeTask:completeTask
+				};
+	}
